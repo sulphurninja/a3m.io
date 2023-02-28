d74c50c30c65ff0bc2b2cefeb67de678a5b680af
sulphurninja <sulphurninja@gmail.com>
2023-02-28 23:20:07 +0530

    second commit

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import RSS from 'rss';
-import { allBlogs } from '.contentlayer/data';
+
 
 async function generate() {
   const feed = new RSS({
@@ -9,15 +9,7 @@ async function generate() {
     feed_url: 'ttp://localhost:3000',
   });
 
-  allBlogs.map((blog) => {
-    feed.item({
-      title: blog.title,
-      url: `http://localhost.com/blog/${blog.slug}`,
-      date: blog.publishedAt,
-      description: blog.description,
-    });
-  });
-
+ 
   writeFileSync('./public/feed.xml', feed.xml({ indent: true }));
 }
 
