553e5617cc8a52eee191b614fe056236d5813dae
sulphurninja <sulphurninja@gmail.com>
2023-03-01 22:32:08 +0530

    added buttons, results table & sounds

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,14 @@ export default function Document() {
         <meta charSet="utf-8" />
         <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
         <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZ32BYXLSG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LZ32BYXLSG');
+        </script>
         <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
         <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
         <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
