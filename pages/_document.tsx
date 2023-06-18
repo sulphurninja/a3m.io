2a631f3653eb1d2e2e9701b3950d614ff6b326c8
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2023-06-18 21:37:37 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,15 +10,6 @@ export default function Document() {
         <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
         <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
         <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-        
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LZ32BYXLSG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LZ32BYXLSG');
-</script>
         <meta content="#FEB48C" name="theme-color" />
         {['PolySans-Regular', 'PolySans-Medium'].map((font) => (
           <link
